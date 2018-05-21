679fa8da249cad84bffeca1b13ef2dd4df333dca
jeffreyfu <fujiayan126@126.com>
2018-05-21 18:52:40 +0800

    Mine first

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+<div id="app">
+  <vue-count-up :end="20000" :duration='10'></vue-count-up>
+</div>
+</template>
+
+<script>
+import vuecountup from './vuecountup.vue'
+export default {
+  components: {
+    'vueCountUp': vuecountup
+  }
+}
+</script>
---
+++ src/vuecountup.vue
@@ -0,0 +1,49 @@
+<template>
+  <span ref='countup'></span>
+</template>
+
+<script type="text/javascript">
+  import CountUp from 'countup.js'
+  export default {
+  data () {
+    return {
+      numAnim:null
+    }
+  },
+   props: {
+            start: {
+                type: Number,
+                default: 0
+            },
+            end: {
+                type: Number,
+                required: !0
+            },
+            decimal: {
+                type: Number,
+                default: 0
+            },
+            duration: {
+                type: Number,
+                default: 2.5
+            },
+            options: {
+                type: Object
+            }
+        },
+  mounted(){
+    this.initCountUp()
+  },
+  methods:{
+    initCountUp(){
+      this.numAnim = new CountUp(this.$refs.countup, this.start,
+                        this.end,
+                        this.decimal,
+                        this.duration,
+                        this.options
+                );
+      this.numAnim.start();
+    }
+  }
+}
+</script>
